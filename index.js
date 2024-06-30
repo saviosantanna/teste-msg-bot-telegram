70292e40cfd45031bdb64af46aa1bb90436c7201
Sávio Sant'anna <saviosantannadesa@gmail.com>
2024-06-30 11:01:58 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch';
+
+// Configurações do bot Telegram
+const botToken = '7486305961:AAHNaYir4Cf4Vq6TFhdMhpULX64OiUHXQpc';
+const chatIds = ['447938340', '6452451093']; // Lista de IDs de chat
+
+// Mensagem que será enviada
+const message = 'Msg de teste para duas pessoas.';
+
+// Função para enviar mensagem via bot do Telegram
+async function sendMessage(chatId) {
+    const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+    const params = new URLSearchParams({
+        chat_id: chatId,
+        text: message,
+    });
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: params,
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erro ao enviar mensagem para o chat_id ${chatId}`);
+        }
+
+        console.log(`Mensagem enviada com sucesso para o chat_id ${chatId}`);
+    } catch (error) {
+        console.error('Erro:', error.message);
+    }
+}
+
+// Executar a função para enviar mensagem para cada chat_id
+async function sendMessagesToMultipleChats() {
+    for (const chatId of chatIds) {
+        await sendMessage(chatId);
+    }
+}
+
+// Chamar a função para enviar mensagem para múltiplos chats
+sendMessagesToMultipleChats();
